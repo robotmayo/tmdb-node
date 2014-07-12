703edae8e016aaa822f7d0b3568294cdb17da7cb
robotmayo <robotmayo@gmail.com>
2014-07-11 22:13:05 -0400

    YOLO HOLO

--- test/tmdbapi.test.js
+++ test/tmdbapi.test.js
@@ -4,6 +4,8 @@ var test = require('tape')
 ,   config = require('../lib/config')
 ,   utils = require('../lib/utils')
 ,   nock = require('nock')
+,   host = 'http://api.themoviedb.org'
+,   apiVer = '/3'
 
 
 test("Throw an error if key is not supplied.", function(t){
@@ -38,38 +40,69 @@ test("Calls the genre endpoints", function(t){
     t.plan(3)
     var api = new Api('key')
 
-    nock('http://api.themoviedb.org')
+    nock(host)
         .get('/3/genre/movie/list?api_key=key')
         .reply(200, {works : 'yes'})
     api.genres(function(err,data,res,opts){
         t.equal(data.works,'yes', 'Getting Genre List')
     })
 
-    nock('http://api.themoviedb.org')
+    nock(host)
         .get('/3/genre/28/movies?api_key=key')
         .reply(200, {genre : 'action'})
     api.genres(28, function(err,data,opts,dd,res){
         t.equal(data.genre, 'action', 'Getting Genre List')
     })
 
-    nock('http://api.themoviedb.org')
+    nock(host)
         .get('/3/genre/28/movies?page=2&api_key=key')
         .reply(200, {genre : 'action'})
     api.genres(28, {page : 2}, function(err,data,opts,dd,res){
-        console.log(err)
         t.equal(data.genre, 'action', 'Getting Genre List')
     })
 })
 
-test("Calls the changes endpoints", function(t){
-    t.plan(1)
+test("Calls the changes endpoints / test typed generic", function(t){
+    t.plan(3)
     var api = new Api("key")
-    nock('http://api.themoviedb.org')
+    nock(host)
         .get('/3/movie/changes?api_key=key')
         .reply(200, {changed : true})
     api.changes(function(err,data,opts,dd,res){
+        t.ok(data.changed, 'Endpoint : Changes with default param')
+    })
+
+    nock(host)
+        .get('/3/tv/changes?api_key=key')
+        .reply(200, {changed : true})
+    api.changes('tv', function(err,data,opts,dd,res){
+        t.ok(data.changed, 'Endpoint : Changes with tv type')
+    })
+
+    nock(host)
+        .get('/3/tv/changes?page=2&api_key=key')
+        .reply(200, {changed : true})
+    api.changes('tv', {page : 2}, function(err,data,opts,dd,res){
+        t.ok(data.changed, 'Endpoint : Changes with options')
+    })
+})
+
+test("Movie chain / chain generic", function(t){
+    t.plan(2)
+    var api = new Api("key")
+    nock(host)
+        .get('/3/movie/550?api_key=key')
+        .reply(200, {changed : true})
+    api.movie(550,function(err,data,opts,dd,res){
+        t.ok(data.changed, 'Endpoint : Chain with id and callback first')
+    })
+
+    nock(host)
+        .get('/3/movie/550?append_to_response=images&api_key=key')
+        .reply(200, {changed : true})
+    api.movie(550).images(function(err,data,opts,dd,res){
         console.log(err)
-        t.equal(data.changed, true, 'Endpoint : Changes with default param')
+        t.ok(data.changed, 'Chained method with callback')
     })
 })
 
